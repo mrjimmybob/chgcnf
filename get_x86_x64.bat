05246d4420a6c45610e5f8ac6db25083772655a8
James Sanchez Seaton Reid <mr_jimmybob@yahoo.com>
2021-03-03 18:00:47 +0000

    test

--- get_x86_x64.bat
+++ get_x86_x64.bat
@@ -2,5 +2,4 @@ DEL /F /Q C:\Users\mark\source\repos\chgcnf3\chgcnf3_x64.exe
 DEL /F /Q C:\Users\mark\source\repos\chgcnf3\chgcnf3_x86.exe
 
 COPY /Y C:\Users\mark\source\repos\chgcnf3\bin\x64\Release\chgcnf3.exe C:\Users\mark\source\repos\chgcnf3\chgcnf3_x64.exe
-
 COPY /Y C:\Users\mark\source\repos\chgcnf3\bin\x86\Release\chgcnf3.exe C:\Users\mark\source\repos\chgcnf3\chgcnf3_x86.exe
